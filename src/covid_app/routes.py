6c126a2f784a375a60133813e004f5d8b63ba336
patbeqo <patbeqo@gmail.com>
2020-07-12 18:09:06 -0700

    Created new function add city data

--- src/covid_app/routes.py
+++ src/covid_app/routes.py
@@ -1,7 +1,11 @@
 from flask import render_template, url_for, redirect
 from covid_app import app, db
 from covid_app.models import City
+
 import folium
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
 
 
 
@@ -15,22 +19,27 @@ def home():
     # Creates map object starting in New York City with Styling applied
     m = folium.Map(width='75%', height='50%', left='12.5%', location=[40.7128, -74.0060], zoom_start=5)
 
-    # Add marker for every capital city (Set to 10 just to test)
-    for i in range(10):
+    # Calls webscrape function to grab all state names and coordinates
+    statesNameCoordinates = getCityNamesCoordinates()
 
-        # Data will be grabbed from db
-        cityName = 'helloworkd'
-        currentCases = 'r'
-        projectedCases = '32'
-        longitude = 42.659829
-        latitude = -73.781339
+    # Add marker for every capital city (Set to 10 just to test)
+    for element in statesNameCoordinates:
+
+        # Function call to grab data from database should be made here
+
+
+        cityName = element[2]
+        currentCases = 'test'
+        projectedCases = 'test'
+        longitude = element[1]
+        latitude = element[3]
 
         # Popup message that will be displayed when user selects city
         msg='<p>Current: {currentCases}</p><p>Projected: {projectedCases}'.format(currentCases=currentCases, projectedCases=projectedCases)
         popup = folium.Popup(msg, max_width=2650)
 
         # Creates a marker ( + i for testing)
-        folium.Marker([longitude + i, latitude + i], popup=popup, tooltip=cityName).add_to(m)
+        folium.Marker([longitude, latitude], popup=popup, tooltip=cityName).add_to(m)
 
     # Creates the HTML file in order to display
     m.save('covid_app/templates/map.html')
@@ -40,5 +49,41 @@ def home():
 
 
 # Web Scrape for City data and add to database
-def update_db():
-    print("hello")
+def getCityNamesCoordinates():
+
+    # Initialize new array store every states name, longitude and latitude
+    states_array = []
+
+    # Location of chromedriver.exe on my personal computer from where I run this program
+    chromedriver = '/mnt/c/Program Files/chromedriver.exe'
+
+    # Below fixes issues with the dynamic webpage replacing HTML content
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+
+    browser= webdriver.Chrome(executable_path=chromedriver, chrome_options=options)
+
+    # Website to scrape from
+    url = "https://developers.google.com/public-data/docs/canonical/states_csv"
+
+    # Grabs HTML from Website in order to parse
+    browser.get(url)
+    content = browser.page_source
+    soup = BeautifulSoup(content, 'html.parser')
+
+    data = soup.find(class_="devsite-table-wrapper").find_all("td")
+    counter = 1
+    tmp_arr = []
+
+    for element in data:
+        wantedText = element.get_text()
+        if counter == 4:
+            tmp_arr.insert(2, wantedText)
+            states_array.insert(len(states_array), tmp_arr)
+            tmp_arr = []
+            counter = 1
+        else:
+            tmp_arr.insert(len(tmp_arr), wantedText)
+            counter += 1
+    
+    return states_array
